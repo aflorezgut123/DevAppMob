3d327762219c251160f2373dfcdbbc120dd168e3
Luis RoaH <luis.roa@avaldigitallabs.com>
2021-10-07 22:46:28 -0500

    Add event onclic two row
    Add event onclic two row then lef seven and  eigth

--- Calculator/app/src/main/java/co/org/poli/calculator/MainActivity.java
+++ Calculator/app/src/main/java/co/org/poli/calculator/MainActivity.java
@@ -2,6 +2,7 @@ package co.org.poli.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void limpiarBTN(View view){
+
+    }
+
+    public void parentesisBTN(View view){
+
+    }
+
+    public void sieteBTN(View view){
+
+    }
+
+    public void ochoBTN(View view){
+
+    }
+
+    public void cuatroBTN(View view){
+
+    }
+
+    public void cincoBTN(View view){
+
+    }
+
+    public void unoBTN(View view){
+
+    }
+
+    public void dosBTN(View view){
+
+    }
+
+    public void plusMinusBTN(View view){
+
+    }
+
+    public void zeroBTN(View view){
+
+    }
+
 }
